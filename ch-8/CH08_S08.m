d14e850a7d625363bcd3f4e2947cad15bd503d8b
kmg7 <kmlgkcy.dev@gmail.com>
2026-01-01 15:02:30 +0300

    ch8: add section 8 sequential design of disk drive read system

---
+++ ch-8/CH08_S08.m
@@ -0,0 +1,21 @@
+
+sys_pd = tf([1 1], 1)
+
+Tao_1 = 10^-3
+sys_motor_coil = tf(5, [Tao_1, 1])
+
+Tao_2 = 1/20
+sys_arm = tf(0.05, conv([1 0], [Tao_2, 1]))
+
+zet = 0.3
+w_n = 18.85e03
+sys_flexure_and_head = tf(1, [w_n^-2 2*zet/w_n 1])
+
+sys_open = minreal(sys_pd * sys_motor_coil * sys_arm * sys_flexure_and_head)
+figure(1); bode(sys_open)
+figure(2); rlocus(sys_open)
+K = 400
+sys = minreal(feedback(K*sys_open, 1))
+figure(3); stepplot(sys); stepinfo(sys)
+figure(4); pzplot(sys)
+figure(5); bode(sys)
